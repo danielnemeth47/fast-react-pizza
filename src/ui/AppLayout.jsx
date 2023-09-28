f3fa41f036e3e19e35b51b82cec4276366071466
Dániel Németh <danielnemeth47@gmail.com>
2023-09-28 15:32:53 +0200

    Adding the tailwind library

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,14 +1,14 @@
-import { Outlet, useNavigation } from "react-router-dom";
-import CartOverview from "../features/cart/CartOverview";
-import Header from "./Header";
-import Loader from "./Loader";
+import { Outlet, useNavigation } from 'react-router-dom';
+import CartOverview from '../features/cart/CartOverview';
+import Header from './Header';
+import Loader from './Loader';
 
 function AppLayout() {
   const navigation = useNavigation();
-  const isLoading = navigation.state === "loading";
+  const isLoading = navigation.state === 'loading';
 
   return (
-    <div className="layout">
+    <div className="grid h-[500px] grid-rows-3 bg-red-500">
       {isLoading && <Loader />}
       <Header />
       <main>
