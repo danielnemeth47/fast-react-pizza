02d9bfcfd927d89ab95865dffb53b00d99e11ad5
Dániel Németh <danielnemeth47@gmail.com>
2023-10-27 12:10:49 +0200

    Skipping Tailwind styling.

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,19 +1,23 @@
-import { Outlet, useNavigation } from 'react-router-dom';
-import CartOverview from '../features/cart/CartOverview';
 import Header from './Header';
 import Loader from './Loader';
+import CartOverview from '../features/cart/CartOverview';
+import { Outlet, useNavigation } from 'react-router-dom';
 
 function AppLayout() {
   const navigation = useNavigation();
   const isLoading = navigation.state === 'loading';
 
   return (
-    <div className="grid h-[500px] grid-rows-3 bg-red-500">
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
       {isLoading && <Loader />}
+
       <Header />
-      <main>
-        <Outlet />
-      </main>
+
+      <div className="overflow-scroll">
+        <main className="mx-auto max-w-3xl">
+          <Outlet />
+        </main>
+      </div>
 
       <CartOverview />
     </div>
